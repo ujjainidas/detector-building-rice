4a511aebca5058eaff4a08e5a8c400ad594fea9a
ujjainidas <43097770+ujjainidas@users.noreply.github.com>
2020-01-02 19:50:05 -0600

    coded everything

--- DetectorBuilding/DetectorBuilding.ino
+++ DetectorBuilding/DetectorBuilding.ino
@@ -1,9 +1,16 @@
-void setup() {
-  // put your setup code here, to run once:
 
+int inputPin = A0;
+int voltage = 0;
+void setup() 
+{
+  // put your setup code here, to run once:
+  //pinMode(A0, OUTPUT);
 }
 
-void loop() {
+void loop() 
+{
   // put your main code here, to run repeatedly:
-
+  voltage = analogRead(inputPin);
+  delay(1000);
+  Serial.println(voltage);
 }
