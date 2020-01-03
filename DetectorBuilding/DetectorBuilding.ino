0c53f286de579bb893ed13e7c096da5ef435985c
Varun Kumar <varun.thegreate.kumar@gmail.com>
2020-01-02 20:35:04 -0600

    code

--- DetectorBuilding/DetectorBuilding.ino
+++ DetectorBuilding/DetectorBuilding.ino
@@ -1,16 +1,23 @@
-
-int inputPin = A0;
-int voltage = 0;
+int inputPin = 0;
+int reading = 0;
 void setup() 
 {
   // put your setup code here, to run once:
-  //pinMode(A0, OUTPUT);
+  Serial.begin(9600);
 }
 
 void loop() 
 {
   // put your main code here, to run repeatedly:
-  voltage = analogRead(inputPin);
-  delay(1000);
+  reading = analogRead(inputPin);
+  float voltage = reading * 5.0;
+  voltage /= 1024.0;
   Serial.println(voltage);
+  float temperatureC = (voltage - 0.5) * 100 ; 
+  Serial.print(temperatureC); Serial.println(" degrees C");
+ 
+ // now convert to Fahrenheit
+ float temperatureF = (temperatureC * 9.0 / 5.0) + 32.0;
+ Serial.print(temperatureF); Serial.println(" degrees F");
+ delay(1000);
 }
